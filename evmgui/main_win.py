43063eeb665ce61ef14651e2f1e7ead85a9b66f8
Replit user <>
2023-11-26 13:06:48 +0000

    Your commit message

--- evmgui/main_win.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-import win_ui
-
-if __name__ == '__main__':
-    app = QApplication([])
-    my_window = win_ui.Main_WIN()
-    my_window.show()
-    app.exec_()
